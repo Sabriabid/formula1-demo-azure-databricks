8926528321ef3bb3eec198e8843c5444747e8c3e
sabriabd.pro@gmail.com <sabriabd.pro@gmail.com>
2024-01-29 14:33:43 +0000

    test py

--- Notebook/test.py
+++ Notebook/test.py
@@ -1,2 +1,21 @@
 # Databricks notebook source
-print("test")
+# MAGIC %md
+# MAGIC Ingest constructors.json file
+
+# COMMAND ----------
+
+constructors_schema = "constructorId INT, constructorREF STRING, name STRING, nationality STRING, url STRING"
+
+# COMMAND ----------
+
+constructor_df = spark.read \
+    .schema(constructors_schema) \
+    .json("/Workspace/Repos/Demo/formula1-demo-azure-databricks/data/raw/raw/constructors.json")
+
+# COMMAND ----------
+
+dbutils.fs.ls("/Repos/Demo/formula1-demo-azure-databricks/data/raw")
+
+# COMMAND ----------
+
+
