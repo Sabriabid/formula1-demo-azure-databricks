2be3c3f045baa557b6325b1e378b13f848c88248
sabriabd.pro@gmail.com <sabriabd.pro@gmail.com>
2024-01-30 14:53:11 +0000

    modifications

--- Notebook/ingest circuit file.py
+++ Notebook/ingest circuit file.py
@@ -9,11 +9,31 @@ display(dbutils.fs.mounts())
 # COMMAND ----------
 
 # MAGIC %fs 
-# MAGIC ls /mnt/raw
+# MAGIC ls /mnt/processed
 
 # COMMAND ----------
 
-circuits_df = spark.read.option("header", True).csv("dbfs:/mnt/raw/circuits.csv")
+from pyspark.sql.types import StructField, StructType, IntegerType, StringType, DoubleType
+
+# COMMAND ----------
+
+circuits_schema = StructType(fields=[StructField("circuitId", IntegerType(), False),
+                                      StructField("circuitref", StringType(), False),
+                                      StructField("name", StringType(), False),
+                                      StructField("location", StringType(), False),
+                                      StructField("country", StringType(), False),
+                                      StructField("lat", DoubleType(), False),
+                                      StructField("lng", DoubleType(), False),
+                                      StructField("alt", IntegerType(), False),
+                                      StructField("url", StringType(), False),
+])
+
+# COMMAND ----------
+
+circuits_df = spark.read \
+    .option("header", True) \
+    .schema(circuits_schema) \
+    .csv("dbfs:/mnt/raw/circuits.csv")
 
 # COMMAND ----------
 
@@ -22,6 +42,62 @@ display(circuits_df)
 # COMMAND ----------
 
 circuits_df.show()
+
+# COMMAND ----------
+
+circuits_df.printSchema()
+
+# COMMAND ----------
+
+circuits_df.describe().show()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+circuits_selected_df = circuits_df.select("circuitId", "circuitRef", "name", "location", "country", "lat", "lng", "alt")
+
+# COMMAND ----------
+
+circuits_renamed_df = circuits_selected_df.withColumnRenamed("circuitId", "circuit_id") \
+    .withColumnRenamed("circuitRef", "circuit_ref") \
+    .withColumnRenamed("lat", "latitude") \
+    .withColumnRenamed("lng", "longitude") \
+    .withColumnRenamed("alt", "altitude")
+
+# COMMAND ----------
+
+display(circuits_renamed_df)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+circuits_final_df = circuits_renamed_df.withColumn("ingestion_date", current_timestamp())
+
+# COMMAND ----------
+
+display(circuits_final_df)
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC ###### Write data to our dataframe
+
+# COMMAND ----------
+
+circuits_final_df.write.mode("overwrite").parquet("/mnt/processed/circuits")
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls /mnt/processed/circuits
+
+# COMMAND ----------
+
+df = spark.read.parquet("/mnt/processed/circuits")
+display(df)
 
 # COMMAND ----------
 
